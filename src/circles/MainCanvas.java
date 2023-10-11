e24675e80368810aa8c58010f1616e1b50f6f18a
NikNik <56563372+Nikkru@users.noreply.github.com>
2023-10-11 17:43:05 +0300

    did without interface

---
+++ src/circles/MainCanvas.java
@@ -0,0 +1,44 @@
+package circles;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainCanvas extends JPanel {
+    private final MainWindow controller;
+    private long lastFrameTime;
+
+    MainCanvas(MainWindow controller) {
+
+//        setBackground(Color.BLUE);
+        this.controller = controller;
+        lastFrameTime = System.nanoTime();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        float deltaTime = (System.nanoTime() - lastFrameTime) * 0.000000001f;
+        controller.onDrawFrame(this, g, deltaTime);
+        lastFrameTime = System.nanoTime();
+        try {
+            Thread.sleep(16);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        repaint();
+    }
+
+    public int getLeft() {
+        return 0;
+    }
+    public int getRight() {
+        return getWidth() - 1;
+    }
+    public int getTop() {
+        return 0;
+    }
+    public int getBottom() {
+        return getHeight() - 1;
+    }
+}
