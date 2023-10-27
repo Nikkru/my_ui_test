8d58785c396a01c1fe8a1167578072dc8cd92f06
NikNik <56563372+Nikkru@users.noreply.github.com>
2023-10-27 18:46:55 +0300

    add task 3
    Class Pair <A, B>

--- src/sem_3/hw/pair/Pair.java
+++ src/sem_3/hw/pair/Pair.java
@@ -1,4 +1,34 @@
 package sem_3.hw.pair;
 
-public class Pair {
+/*
+Напишите обобщенный класс Pair, который представляет собой пару значений разного типа.
+Класс должен иметь методы getFirst(), getSecond() для получения значений каждого из составляющих пары,
+а также переопределение метода toString(), возвращающее строковое представление пары.
+ */
+
+public class Pair<A, B> {
+    private A a;
+    private B b;
+
+    public Pair(A a, B b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public A getA() {
+        return a;
+    }
+
+    public B getB() {
+        return b;
+    }
+
+    @Override
+    public String toString() {
+        return "( first element " + a + " second element " + b;
+    }
+
+    public static void main(String[] args) {
+        Pair<Integer, String> pair = new Pair<>(10, "ten");
+    }
 }
