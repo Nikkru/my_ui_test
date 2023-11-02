eacc5e1e5e72377312c5afc67c5dac24c4fbce46
NikNik <56563372+Nikkru@users.noreply.github.com>
2023-11-02 08:53:10 +0300

    add PlaceAtTheTable

---
+++ src/sem_5/homework/PlaceAtTheTable.java
@@ -0,0 +1,13 @@
+package sem_5.homework;
+
+public class PlaceAtTheTable {
+    Philosopher philosopher;
+    Forks forks1;
+    Forks forks2;
+
+    public PlaceAtTheTable(Philosopher philosopher, Forks forks1, Forks forks2) {
+        this.philosopher = philosopher;
+        this.forks1 = forks1;
+        this.forks2 = forks2;
+    }
+}
