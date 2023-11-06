d5b2af2ba6864f5b2753aadda144bb737f9efd67
NikNik <56563372+Nikkru@users.noreply.github.com>
2023-11-06 20:17:30 +0300

    fix Main 2

--- src/sem_5/hw_Semaphor_1/Main.java
+++ src/sem_5/hw_Semaphor_1/Main.java
@@ -1,17 +1,17 @@
 package sem_5.hw_Semaphor_1;
 
 public class Main {
-    private static final int N = 5;
-    private static final int DINNERSCOUNT = 3;
-    private static Philosopher[] philosophers = new Philosopher[N];
-    private static Fork[] forks = new Fork[N];
+    private static final int NUMBEROFPHILOSIPHERS = 5;
+    private static final int NUMBEROFDINNERS = 3;
+    private static Philosopher[] philosophers = new Philosopher[NUMBEROFPHILOSIPHERS];
+    private static Fork[] forks = new Fork[NUMBEROFPHILOSIPHERS];
 
     public static void main(String[] args) {
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < NUMBEROFPHILOSIPHERS; i++) {
             forks[i] = new Fork(i);
         }
-        for (int i = 0; i < N; i++) {
-            philosophers[i] = new Philosopher(i, DINNERSCOUNT, forks[i], forks[(i + 1) % N]);
+        for (int i = 0; i < NUMBEROFPHILOSIPHERS; i++) {
+            philosophers[i] = new Philosopher(i, NUMBEROFDINNERS, forks[i], forks[(i + 1) % NUMBEROFPHILOSIPHERS]);
             philosophers[i].start();
         }
     }
