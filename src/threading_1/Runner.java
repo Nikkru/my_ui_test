5a752451cf55977c536b736e6d2a27efb61a0dae
NikNik <56563372+Nikkru@users.noreply.github.com>
2023-11-08 22:25:29 +0300

    Runnable () ->

--- src/threading_1/Runner.java
+++ src/threading_1/Runner.java
@@ -2,17 +2,22 @@ package threading_1;
 
 import static java.lang.System.out;
 import static java.lang.Thread.currentThread;
+import static java.util.stream.IntStream.range;
 
 public class Runner {
-    public static void main(String[] args) {
-        out.println(currentThread().getName());
+    private  static final int CREATED_THREADS_AMOUNT = 10;
 
-        final Thread thread = new Thread() {
-            @Override
-            public void run() {
-                out.println(currentThread().getName());
-            }
-        };
+    public static void main(String[] args) {
+        final Runnable task = () -> out.println(currentThread().getName());
+//        final Thread thread = new Thread(task);
+//        thread.start();
+        final Runnable taskCreatingThread = () -> range(0, CREATED_THREADS_AMOUNT)
+                .forEach(i -> startThread(task));
+        startThread(taskCreatingThread);
+    }
+
+    private static  void startThread(final Runnable runnable) {
+        final Thread thread = new Thread(runnable);
         thread.start();
     }
 }
